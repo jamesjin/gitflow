aa5c6c41b0e27ca15c8fd8616adf03c1b28c3989
James Jin <james.king.zj@gmail.com>
2014-12-23 23:50:35 +0800

    commit2

---
+++ src/main/java/com/jamesjin/example/Music.java
@@ -0,0 +1,15 @@
+package com.jamesjin.example;
+
+public class Music {
+
+    private int commit2;
+
+    public int getCommit2() {
+        return commit2;
+    }
+
+    public void setCommit2(int commit2) {
+        this.commit2 = commit2;
+    }
+
+}
