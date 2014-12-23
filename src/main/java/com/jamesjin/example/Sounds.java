e15419e935f9ca230a94d2281a3fe140048f3a1a
James Jin <james.king.zj@gmail.com>
2014-12-23 23:49:45 +0800

    commit1

--- src/main/java/com/jamesjin/example/Sounds.java
+++ src/main/java/com/jamesjin/example/Sounds.java
@@ -4,5 +4,6 @@ public class Sounds {
     public static void main(String[] args) {
         System.out.println("no conflicts");
         System.out.println("that's awesome");
+        System.out.println("commit1");
     }
 }
